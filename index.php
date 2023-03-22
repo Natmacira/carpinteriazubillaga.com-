15d60394e390ca343b8d579d1717bc19987943a8
natmacira <natimciraolo@gmail.com>
2023-03-22 18:19:22 -0300

    final texts for history section

--- index.php
+++ index.php
@@ -5,179 +5,185 @@ $body_class = "";
 require_once 'header.php';
 ?>
 <section class="swiper generic-swiper">
-			<div class="swiper-wrapper">
-				<!-- SLIDE 1 -->
-				<article class="swiper-slide main-top">
-					<div class="content-div">
-						<h1>Mucho más que <br> una puerta, <br><span> sumate a la <br> excelencia</span></h1>
-						<a href="">+ info</a></figcaption>
-					</div>
-					<picture>
-						<!-- Desktop -->
-						<source media="(min-width:768px)" srcset="img/desktop/home_desktop.png"
-							alt="Logo Carpinteria Zubillaga">
-						<!-- Mobile  -->
-						<img src="img/mobile/home_mobile.png" alt="">
-					</picture>
-				</article>
-
-				<!-- SLIDE 2 -->
-				<article class="swiper-slide history-slide">
-					<div class="content-div">
-						<h1>+ 130 años <br><span> EN MAR DEL PLATA</span></h1>
-						<a href="https://www.youtube.com/watch?v=KHe4EAMsJ-s&t=11s" target="_blank">+ info</a></figcaption>
-					</div>
-					<picture>
-						<!-- Desktop -->
-						<source media="(min-width:768px)" srcset="img/desktop_home_2.png"
-							alt="Logo Carpinteria Zubillaga">
-						<!-- Mobile  -->
-						<img src="img/mobile_home_2.png" alt="">
-					</picture>
-				</article>
-
-				<!-- SLIDE 3 -->
-				<article class="swiper-slide know-us-more">
-					<div class="content-div">
-						<h1>Conócenos <br> un poco más</h1>
-						<a href="#video">+ info</a></figcaption>
-					</div>
-					<picture>
-						<!-- Desktop -->
-						<source media="(min-width:768px)" srcset="img/desktop_home_3.png"
-							alt="Logo Carpinteria Zubillaga">
-						<!-- Mobile  -->
-						<img src="img/mobile_home_3.png" alt="">
-					</picture>
-				</article>
+	<div class="swiper-wrapper">
+		<!-- SLIDE 1 -->
+		<article class="swiper-slide main-top">
+			<div class="content-div">
+				<h1>Mucho más que <br> una puerta, <br><span> sumate a la <br> excelencia</span></h1>
+				<a href="">+ info</a></figcaption>
 			</div>
-			<div class="swiper-pagination"></div>
+			<picture>
+				<!-- Desktop -->
+				<source media="(min-width:768px)" srcset="img/desktop/home_desktop.png" alt="Logo Carpinteria Zubillaga">
+				<!-- Mobile  -->
+				<img src="img/mobile/home_mobile.png" alt="">
+			</picture>
+		</article>
 
-		</section>
-		<section class="history-section" id="history">
-			<button id="slide">+</button>
-			<article class="history-article">
-				<h3>DESDE 1890</h3>
-				<h4>Ramón Zubillaga</h4>
-				<p>Eran épocas de herramientas manuales y nobles maderas macizas (roble eslavonia, cedro paraguayo) que
-					se desembarcaban en el antiguo puerto de Mar del Plata y se transportaban en carretones hasta el
-					taller. Fue en el año 1891 cuando Ramón Zubillaga instaló su taller de carpintería en La Rioja al
-					2100 (hoy centro), Mar del Plata. En aquel primer edificio comenzó a elaborar en forma artesanal,
-					carpintería para importantísimos y típicos chalets marplatenses.</p>
-			</article>
-			<article class="history-article-hnos">
-				<h4>Zubillaga Hnos</h4>
-				<p>Eran épocas de herramientas manuales y nobles maderas macizas (roble eslavonia, cedro paraguayo) que
-					se desembarcaban en el antiguo puerto de Mar del Plata y se transportaban en carretones hasta el
-					taller. Fue en el año 1891 cuando Ramón Zubillaga instaló su taller de carpintería en La Rioja al
-					2100 (hoy centro), Mar del Plata. En aquel primer edificio comenzó a elaborar en forma artesanal,
-					carpintería para importantísimos y típicos chalets marplatenses.</p>
-			</article>
-			<article class="history-article-raul">
-				<h4>Raúl Zubillaga</h4>
-				<p>Eran épocas de herramientas manuales y nobles maderas macizas (roble eslavonia, cedro paraguayo) que
-					se desembarcaban en el antiguo puerto de Mar del Plata y se transportaban en carretones hasta el
-					taller. Fue en el año 1891 cuando Ramón Zubillaga instaló su taller de carpintería en La Rioja al
-					2100 (hoy centro), Mar del Plata. En aquel primer edificio comenzó a elaborar en forma artesanal,
-					carpintería para importantísimos y típicos chalets marplatenses.</p>
-			</article>
-			<article class="history-article-diego">
-				<h4>Diego Zubillaga</h4>
-				<p>Eran épocas de herramientas manuales y nobles maderas macizas (roble eslavonia, cedro paraguayo) que
-					se desembarcaban en el antiguo puerto de Mar del Plata y se transportaban en carretones hasta el
-					taller. Fue en el año 1891 cuando Ramón Zubillaga instaló su taller de carpintería en La Rioja al
-					2100 (hoy centro), Mar del Plata. En aquel primer edificio comenzó a elaborar en forma artesanal,
-					carpintería para importantísimos y típicos chalets marplatenses.</p>
-			</article>
-		</section>
-		<section class="doors-section" id="nobles">
-			<figure>
-				<picture>
-					<!-- Desktop -->
-					<source media="(min-width:768px)" srcset="img/desktop/enchapada_desktop.jpg"
-						alt="Logo Carpinteria Zubillaga">
-					<!-- Mobile  -->
-					<img src="img/mobile/enchapadas_mobile.jpg"
-						alt="Foto de cerca de una puerta enchapada. Manubrio metalizado y cuerpo de color madera.">
-				</picture>
-				<figcaption>
-					<h2>Puerta <br> Enchapada</h2>
-					<a href="enchapada.php">+ info</a>
-				</figcaption>
-			</figure>
-			<figure>
-				<picture>
-					<!-- Desktop -->
-					<source media="(min-width:768px)" srcset="img/desktop/pantografiada_desktop-01.jpg"
-						alt="Logo Carpinteria Zubillaga">
-					<!-- Mobile  -->
-					<img src="img/mobile/pantografiada_mobile.jpg"
-						alt="Foto de cerca de una puerta enchapada. Manubrio metalizado y cuerpo de color madera.">
-				</picture>
-				<figcaption class="left-side">
-					<h2>Puerta <br> Pantografiada</h2>
-					<a href="pantografiada.php">+ info</a>
-				</figcaption>
-			</figure>
-			<figure>
-				<picture>
-					<!-- Desktop -->
-					<source media="(min-width:768px)" srcset="img/desktop/bunada_desktop.jpg"
-						alt="Logo Carpinteria Zubillaga">
-					<!-- Mobile  -->
-					<img src="img/mobile/bunada_mobile.jpg"
-						alt="Foto de cerca de una puerta enchapada. Manubrio metalizado y cuerpo de color madera.">
-				</picture>
+		<!-- SLIDE 2 -->
+		<article class="swiper-slide history-slide">
+			<div class="content-div">
+				<h1>+ 130 años <br><span> EN MAR DEL PLATA</span></h1>
+				<a href="https://www.youtube.com/watch?v=KHe4EAMsJ-s&t=11s" target="_blank">+ info</a></figcaption>
+				<img src="img/desktop/trinagulo_desktop.png" alt="">
+			</div>
+			<picture>
+				<!-- Desktop -->
+				<source media="(min-width:768px)" srcset="img/desktop_home_2.png" alt="Logo Carpinteria Zubillaga">
+				<!-- Mobile  -->
+				<img src="img/mobile_home_2.png" alt="">
+			</picture>
+		</article>
 
-				<figcaption>
-					<h2>Puerta Buñada</h2>
-					<a href="bunada.php">+ info</a>
-				</figcaption>
-			</figure>
-			<figure>
-				<picture>
-					<!-- Desktop -->
-					<source media="(min-width:768px)" srcset="img/desktop/lisa_desktop.jpg"
-						alt="Logo Carpinteria Zubillaga">
-					<!-- Mobile  -->
-					<img src="img/mobile/lisa_mobile.jpg"
-						alt="Foto de cerca de una puerta enchapada. Manubrio metalizado y cuerpo de color madera.">
-				</picture>
-				<figcaption class="left-side">
-					<h2>Puerta Lisa</h2>
-					<a href="lisa.php">+ info</a>
-				</figcaption>
-			</figure>
-			<figure>
-				<picture>
-					<!-- Desktop -->
-					<source media="(min-width:768px)" srcset="img/desktop/hidden_desktop.jpg"
-						alt="Logo Carpinteria Zubillaga">
-					<!-- Mobile  -->
-					<img src="img/mobile/hidden_mobile.jpg"
-						alt="Foto de cerca de una puerta enchapada. Manubrio metalizado y cuerpo de color madera.">
-				</picture>
-				<figcaption>
-					<h2>Puerta Hidden</h2>
-					<a href="hidden.php">+ info</a>
-				</figcaption>
-			</figure>
-			<figure>
-				<picture>
-					<!-- Desktop -->
-					<source media="(min-width:768px)" srcset="img/desktop/moldura_desktop.jpg"
-						alt="Logo Carpinteria Zubillaga">
-					<!-- Mobile  -->
-					<img src="img/mobile/moldura_mobile.jpg"
-					alt="Foto de cerca de una puerta enchapada. Manubrio metalizado y cuerpo de color madera.">
-				</picture>
-				<figcaption class="left-side">
-					<h2>Puerta <br> con moldura aplicada</h2>
-					<a href="moldura-aplicada.php">+ info</a>
-				</figcaption>
-			</figure>
-		</section>
-	
+		<!-- SLIDE 3 -->
+		<article class="swiper-slide know-us-more">
+			<div class="content-div">
+				<h1>Conócenos <br> un poco más</h1>
+				<a href="#video">+ info</a></figcaption>
+				<img src="img/desktop/trinagulo_desktop.png" alt="">
+			</div>
+			<picture>
+				<!-- Desktop -->
+				<source media="(min-width:768px)" srcset="img/desktop_home_3.png" alt="Logo Carpinteria Zubillaga">
+				<!-- Mobile  -->
+				<img src="img/mobile_home_3.png" alt="">
+			</picture>
+		</article>
+	</div>
+	<div class="swiper-pagination"></div>
+	<img src="img/desktop/trinagulo_desktop.png" alt="" class="arrow">
+
+</section>
+<section class="history-section" id="history">
+
+	<button id="slide">+</button>
+	<article class="history-article">
+		<h3>Las puertas de madera tienen apellido</h3>
+		<h4>1890-1920 – Ramón Zubillaga</h4>
+		<p>Eran épocas de <strong> herramientas manuales y nobles maderas macizas</strong>
+			(roble eslavonia, cedro paraguayo) que se desembarcaban en el
+			antiguo puerto de Mar del Plata y se transportaban en carretones
+			hasta el taller. Fue en el año 1891 cuando <strong> Ramón Zubillaga </strong>instaló
+			su taller de carpintería en La Rioja al 2100 (hoy centro), Mar del Plata.
+			En aquel primer edificio comenzó a elaborar en forma artesanal, carpintería para
+			importantísimos y típicos <strong> chalets marplatenses.</strong></p>
+	</article>
+	<article class="history-article-hnos">
+		<h4>1920-1960 – Los Seis Zetas</h4>
+		<p>Casado con María Durquet, sus cinco hijos hicieron eco de su actividad.
+			La <strong> innovación permanente </strong> fue a la vez que <strong> pasión, oficio.</strong> En 1920 se los conoció por su
+			marca <strong> 6 Zetas</strong> (seis Zubillaga). Fundaron un pueblo en Quillén (Río Negro). Allí maquinaron la
+			madera que luego trajeron a Mar del Plata. Del quehacer cotidiano sobrevino la invención: fueron
+			creadores de la primera y revolucionaria <strong> puerta placa</strong>, que reemplazaría la típica puerta maciza
+			de tablero. Elaboraron también el primer <strong>tablero de madera maciza </strong> para fabricar muebles. Los combinados
+			Philco, los muebles de <strong>Mapple</strong> y los gabinetes de las máquinas de coser Singer, entre otros, utilizaron la
+			placa maciza 6 Zetas, que se impuso como un producto de <strong>alta innovación</strong> en la época.</p>
+	</article>
+	<article class="history-article-raul">
+		<h4>1960-2000 – Raúl Zubillaga</h4>
+		<p>
+			Hacia 1970 llegó la tercera generación.
+			<strong>Raúl Zubillaga</strong> instaló una moderna <strong>planta industrial</strong> en San Salvador y Ratery.
+			Allí se inició la modernización: se observó el <strong>confort y la seguridad de los trabajadores</strong>
+			y las <strong>nuevas tendencias mundiales</strong> se abrieron paso. Se perfeccionaron conceptos técnicos para
+			la fabricación de <strong>puertas placa enchapadas</strong> de muy alta calidad y a nivel masivo. Aquellas nuevas
+			puertas placa 6 Zetas, siguen jerarquizando hoy, los edificios más importantes de la ciudad.
+		</p>
+	</article>
+	<article class="history-article-diego">
+		<h4>2000-actualidad – Diego Zubillaga</h4>
+		<p>A mediados de los noventa se sumó la cuarta generación con el<strong> Arq. Diego Zubillaga.</strong> La empresa logró su máximo nivel
+			de precisión tecnológica para la fabricación de puertas de alta calidad.
+			La participación de <strong> Pablo</strong>, y el paso de <strong>Hernán, Luciano y Laura Zubillaga</strong> incrementaron la capacidad de respuesta a
+			diseñadores de primera línea.
+			En 2000, <strong>Zubillaga Empresa de Carpintería es condecorada</strong> por la Handwerkskammer Rheinhessen en la ciudad de <strong>Mainz,
+				Alemania</strong>, por su notable desarrollo empresarial.
+			En 2012 se inaugura la nueva planta industrial, proyectada especialmente para la producción de sofisticadas aberturas
+			interiores e madera.
+			En la actualidad, <strong>ZUBILLAGA PUERTAS NOBLES</strong> realiza una permanente capacitación de su personal, infundiendo el espíritu
+			y las técnicas que hacen a la marca.
+			Sigue incorporando tecnología electrónica de punta que actualiza su crecimiento sostenido.
+			Sigue abasteciendo la construcción horizontal y unifamiliar de alta gama con puertas de vanguardia.
+			Se ha posicionado como <strong>REFERENTE INDISCUTIBLE</strong> de calidad e innovación en la fabricación de <strong>PUERTAS INTERIORES DE CALIDAD.</strong>
+		</p>
+	</article>
+</section>
+<section class="doors-section" id="nobles">
+	<figure>
+		<picture>
+			<!-- Desktop -->
+			<source media="(min-width:768px)" srcset="img/desktop/enchapada_desktop.jpg" alt="Logo Carpinteria Zubillaga">
+			<!-- Mobile  -->
+			<img src="img/mobile/enchapadas_mobile.jpg" alt="Foto de cerca de una puerta enchapada. Manubrio metalizado y cuerpo de color madera.">
+		</picture>
+		<figcaption>
+			<h2>Puerta <br> Enchapada</h2>
+			<a href="enchapada.php">+ info</a>
+		</figcaption>
+	</figure>
+	<figure>
+		<picture>
+			<!-- Desktop -->
+			<source media="(min-width:768px)" srcset="img/desktop/pantografiada_desktop-01.jpg" alt="Logo Carpinteria Zubillaga">
+			<!-- Mobile  -->
+			<img src="img/mobile/pantografiada_mobile.jpg" alt="Foto de cerca de una puerta enchapada. Manubrio metalizado y cuerpo de color madera.">
+		</picture>
+		<figcaption class="left-side">
+			<h2>Puerta <br> Pantografiada</h2>
+			<a href="pantografiada.php">+ info</a>
+		</figcaption>
+	</figure>
+	<figure>
+		<picture>
+			<!-- Desktop -->
+			<source media="(min-width:768px)" srcset="img/desktop/bunada_desktop.jpg" alt="Logo Carpinteria Zubillaga">
+			<!-- Mobile  -->
+			<img src="img/mobile/bunada_mobile.jpg" alt="Foto de cerca de una puerta enchapada. Manubrio metalizado y cuerpo de color madera.">
+		</picture>
+
+		<figcaption>
+			<h2>Puerta Buñada</h2>
+			<a href="bunada.php">+ info</a>
+		</figcaption>
+	</figure>
+	<figure>
+		<picture>
+			<!-- Desktop -->
+			<source media="(min-width:768px)" srcset="img/desktop/lisa_desktop.jpg" alt="Logo Carpinteria Zubillaga">
+			<!-- Mobile  -->
+			<img src="img/mobile/lisa_mobile.jpg" alt="Foto de cerca de una puerta enchapada. Manubrio metalizado y cuerpo de color madera.">
+		</picture>
+		<figcaption class="left-side">
+			<h2>Puerta Lisa</h2>
+			<a href="lisa.php">+ info</a>
+		</figcaption>
+	</figure>
+	<figure>
+		<picture>
+			<!-- Desktop -->
+			<source media="(min-width:768px)" srcset="img/desktop/hidden_desktop.jpg" alt="Logo Carpinteria Zubillaga">
+			<!-- Mobile  -->
+			<img src="img/mobile/hidden_mobile.jpg" alt="Foto de cerca de una puerta enchapada. Manubrio metalizado y cuerpo de color madera.">
+		</picture>
+		<figcaption>
+			<h2>Puerta Hidden</h2>
+			<a href="hidden.php">+ info</a>
+		</figcaption>
+	</figure>
+	<figure>
+		<picture>
+			<!-- Desktop -->
+			<source media="(min-width:768px)" srcset="img/desktop/moldura_desktop.jpg" alt="Logo Carpinteria Zubillaga">
+			<!-- Mobile  -->
+			<img src="img/mobile/moldura_mobile.jpg" alt="Foto de cerca de una puerta enchapada. Manubrio metalizado y cuerpo de color madera.">
+		</picture>
+		<figcaption class="left-side">
+			<h2>Puerta <br> con moldura aplicada</h2>
+			<a href="moldura-aplicada.php">+ info</a>
+		</figcaption>
+	</figure>
+</section>
+
 
 <!-- </body> -->
 <?php
