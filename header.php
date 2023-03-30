a4c068a69da7d7f07252b88d02d3c9901c406a3d
natmacira <natimciraolo@gmail.com>
2023-03-30 02:42:21 -0300

    added favicon test

--- header.php
+++ header.php
@@ -41,39 +41,37 @@ if (
 	<script src="js/burger-menu.js"></script>
 	<script src="js/swiper-bundle.min.js"></script>
 	<script src="js/script.js"></script>
-	<!-- <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192" href="img/favicon/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="img/favicon/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png"> -->
-	<!-- <link rel="manifest" href="img/favicon/manifest.json"> -->
-	<!-- <meta name="msapplication-TileColor" content="#0066ff">
+	<link rel="shortcut icon" href="assets/images/favicon.ico">
+	<link rel="apple-touch-icon" sizes="57x57" href="img/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="img/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="img/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="img/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="img/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="img/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="img/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="img/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+	<link rel="manifest" href="img/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
 	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-	<meta name="theme-color" content="#0066ff">
-	<meta itemProp="image" property="og:image" content="https://nodo.org.ar/img/favicon/android-icon-192x192.png" />
-	<meta property="og:description" content="Combatimos la inseguridad alimentaria impactando positivamente en el medio ambiente.">
-	<meta property="og:locale" content="es_LA">
-	<meta property="og:image" content="https://nodo.org.ar/img/favicon/android-icon-192x192.png" />
+	<meta name="theme-color" content="#ffffff">
 	<meta property="og:type" content="website">
-	<meta property="og:url" content="https://nodo.org.ar/" /> -->
+	<meta property="og:url" content="https://zubillagapuertasnobles.com/" />
 </head>
 
-<body <?php if ( basename( $_SERVER['SCRIPT_FILENAME'] ) !== "index.php" ) { echo 'class="puerta"'; } ?>>
-<header>
+<body <?php if (basename($_SERVER['SCRIPT_FILENAME']) !== "index.php") {
+			echo 'class="puerta"';
+		} ?>>
+	<header>
 		<section class="principal-header-section">
 			<a href="index.php">
 				<picture>
 					<!-- Desktop -->
-					<source media="(min-width:1200px)" srcset="img/logo-web.svg"
-						alt="Logo Carpinteria Zubillaga">
+					<source media="(min-width:1200px)" srcset="img/logo-web.svg" alt="Logo Carpinteria Zubillaga">
 					<!-- Mobile  -->
 					<img src="img/logo-web.svg" alt="Logo Carpinteria Zubillaga">
 				</picture>
